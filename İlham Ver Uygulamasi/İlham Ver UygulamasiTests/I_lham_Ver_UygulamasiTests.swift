a342efed9b978b00d3049de944b64cf67297c07e
Tuğba <56814718+Tugbagulsen@users.noreply.github.com>
2022-11-09 17:26:56 +0300

    ok

---
+++ İlham Ver Uygulamasi/İlham Ver UygulamasiTests/I_lham_Ver_UygulamasiTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_lham_Ver_UygulamasiTests.swift
+//  İlham Ver UygulamasiTests
+//
+//  Created by Kasım Adalan on 20.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import XCTest
+@testable import I_lham_Ver_Uygulamasi
+
+class I_lham_Ver_UygulamasiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
