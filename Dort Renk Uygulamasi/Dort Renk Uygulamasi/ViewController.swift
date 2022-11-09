a9252d4c2118a0233d097cdc91a2c199f6950598
Tuğba <56814718+Tugbagulsen@users.noreply.github.com>
2022-11-09 17:28:12 +0300

    ok

---
+++ Dort Renk Uygulamasi/Dort Renk Uygulamasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dort Renk Uygulamasi
+//
+//  Created by Kasım Adalan on 19.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
