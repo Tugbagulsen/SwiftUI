37ab1f882ae043767007d81a62521c58d0428bf8
Tuğba <56814718+Tugbagulsen@users.noreply.github.com>
2022-11-09 17:26:06 +0300

    ok

---
+++ Yemek Tarifi Uygulamasi/Yemek Tarifi Uygulamasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yemek Tarifi Uygulamasi
+//
+//  Created by Kasım Adalan on 20.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
