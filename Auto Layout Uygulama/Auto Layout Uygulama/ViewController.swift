9b8a5ad166c7a5edb5961abf21c694ce74a33f3b
Tuğba <56814718+Tugbagulsen@users.noreply.github.com>
2022-11-09 17:28:35 +0300

    ok

---
+++ Auto Layout Uygulama/Auto Layout Uygulama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Uygulama
+//
+//  Created by Kasım Adalan on 19.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
